24818f70c5063d62636607441e4ba3ed3528c5b6
Vanessa Neves <113128675+vanessaneves-dev@users.noreply.github.com>
2025-03-13 21:17:01 -0300

    :sparkles: ChessException and ChessPosition

--- src/aplication/Main.java
+++ src/aplication/Main.java
@@ -3,8 +3,6 @@ package aplication;
 
 import chess.ChessMatch;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
